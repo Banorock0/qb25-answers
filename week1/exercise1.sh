a2d61b04cd3889cef2256073fb3c8359df0a5d5a
Banorock0 <jander@jkugelman.com>
2025-09-12 11:26:00 -0400

    exercise1

---
+++ week1/exercise1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Maketools command
+bedtools makewindows -g hg19-main.chrom.sizes -w 1000000
+
+# Intersect command
+bedtools intersect -c -a hg19-1mb.bed -b hg19-kc.bed > hg19-kc-count.bed
