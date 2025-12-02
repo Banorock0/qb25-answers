034efb653d444804690141a159ac924f42316ee0
Banorock0 <jander@jkugelman.com>
2025-12-01 22:14:53 -0500

    week11

--- week11/ex1.sh
+++ week11/ex1.sh
@@ -2,15 +2,15 @@
 
 # 3x coverage
 echo "3x coverage:"
-python week11.py 3
-Rscript week11.R data/genome_cov_3x.txt 3 3 1.73
+python ex1.py 3
+Rscript ex1.R data/genome_cov_3x.txt 3 3 1.73
 
 # 10x coverage
 echo "10x coverage:"
-python week11.py 10
-Rscript week11.R data/genome_cov_10x.txt 10 10 3.16
+python ex1.py 10
+Rscript ex1.R data/genome_cov_10x.txt 10 10 3.16
 
 # 30x coverage
 echo "30x coverage:"
-python week11.py 30
-Rscript week11.R data/genome_cov_30x.txt 30 30 5.47
+python ex1.py 30
+Rscript ex1.R data/genome_cov_30x.txt 30 30 5.47
